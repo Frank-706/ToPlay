8fcd1a7a270987784d214c4c22e8c943f64e708d
Frank-706 <81532465+Frank-706@users.noreply.github.com>
2021-04-27 22:40:12 +0200

    Update confirm.php

--- confirm.php
+++ confirm.php
@@ -1,47 +1,64 @@
-<?php
-include "config.php";
-//$numero = rand(1,100);
-//echo $_GET['n_casuale'];
-//session_start();
-$CN = new mysqli($CONFIG["db-host"], $CONFIG["db-user"], $CONFIG["db-paswd"], $CONFIG["db-name"]);
-if ($CN->connect_error)
-	die("Connection failed: " . $CN->connect_error);
-//if(isset($_SESSION['email'])){
-    //var_dump($_SESSION);
-    $n=$_GET['n_casuale'];
-    //$email=$_GET['email'];
-  $sql = "update tp_utenti r set r.is_enabled=1 where r.registrazione='$n'";
-  //echo $sql;
-  $CN->query($sql); // esecuzione della query di insert sul DB
-  $msg="Conferma avvenuta con successo";
-  //echo $msg.'<br>';     
-//}
-
-/*else{
-    $msg= 'Nessuna sessione';
-}*/
-?>
-<!DOCTYPE html>
-<html>
-<title>REGISTRAZIONE : Confirm </title>
-<head> 
-        <style>
-        	body {
-            	padding:10px;
-            }  
-        </style>
-</head>
-<body>
-<h1>Registrazione</h1>
-<?php
-// connessione al DB
-  //echo $_SESSION['email'];
-    //echo $sql;
-  echo $msg;
-  ?>
-  <br><a href="http://frankmoses.altervista.org/wapp/toplay/index.php">Torna alla home del catalogo</a>
-<?php
-    $CN->close();
-?>
-</body>
-</html>
+<?php
+include "config.php";
+$CN = new mysqli($CONFIG["db-host"], $CONFIG["db-user"], $CONFIG["db-paswd"], $CONFIG["db-name"]);
+
+if ($CN->connect_error)
+
+	die("Connection failed: " . $CN->connect_error);
+    $n=$_GET['n_casuale'];
+  $sql = "update tp_utenti r set r.is_enabled=1 where r.registrazione='$n'";
+
+  //echo $sql;
+
+  $CN->query($sql); // esecuzione della query di insert sul DB
+
+  $msg="Conferma avvenuta con successo";
+?>
+
+<!DOCTYPE html>
+
+<html>
+
+<title>REGISTRAZIONE : Confirm </title>
+
+<head> 
+
+        <style>
+
+        	body {
+
+            	padding:10px;
+
+            }  
+
+        </style>
+
+</head>
+
+<body>
+
+<h1>Registrazione</h1>
+
+<?php
+
+// connessione al DB
+
+  //echo $_SESSION['email'];
+
+    //echo $sql;
+
+  echo $msg;
+
+  ?>
+
+  <br><a href="http://frankmoses.altervista.org/wapp/toplay/index.php">Torna alla home del catalogo</a>
+
+<?php
+
+    $CN->close();
+
+?>
+
+</body>
+
+</html>
